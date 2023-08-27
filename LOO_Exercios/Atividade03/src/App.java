df03004d07c7d4cc40da090e70a9c0ff71bc51b9
FelipeHalls <feliperauzer16@gmail.com>
2023-08-27 16:22:46 -0400

    Atividade 03

--- LOO_Exercios/Atividade03/src/App.java
+++ LOO_Exercios/Atividade03/src/App.java
@@ -1,5 +1,19 @@
+import java.time.LocalDate;
+
+import uniderp.loo.escola.dominio.aluno;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        aluno C1 = new aluno();
+        C1.setCodigo(1);
+        C1.setNome("Felipe");
+        C1.setEndereco("Rua píratininga, 777");
+        C1.setTelefone("67992884553");
+        C1.setDataNascimento(LocalDate.of(2010, 12, 31));
+        C1.setRg("2225481");
+        C1.setCpf("82448130524");
+        C1.setDataMatricula(LocalDate.now());
+        C1.setMatricula("222");
     }
 }
